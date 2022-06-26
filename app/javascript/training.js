0f3ad69558ce9bacbd6d508556dcb62b6133f5d8
piroyuki666 <shtr3636@gmail.com>
2022-06-26 11:30:41 +0900

    トレーニング項目に反映

--- app/javascript/training.js
+++ app/javascript/training.js
@@ -21,4 +21,44 @@ const trainingsHash = $('#trainings').data('trainings');
     $('#selected-persons').text(persons);
     $('#selected-content').text(content);
   });
+  $('#training2').on('click',function(){
+    const title = (trainingsHash[1].title);
+    const time = (trainingsHash[1].time);
+    const persons = (trainingsHash[1].persons);
+    const content = (trainingsHash[1].content);
+    $('#selected-title').text(title);
+    $('#selected-time').text(time);
+    $('#selected-persons').text(persons);
+    $('#selected-content').text(content);
+  });
+  $('#training3').on('click',function(){
+    const title = (trainingsHash[2].title);
+    const time = (trainingsHash[2].time);
+    const persons = (trainingsHash[2].persons);
+    const content = (trainingsHash[2].content);
+    $('#selected-title').text(title);
+    $('#selected-time').text(time);
+    $('#selected-persons').text(persons);
+    $('#selected-content').text(content);
+  });
+  $('#training4').on('click',function(){
+    const title = (trainingsHash[3].title);
+    const time = (trainingsHash[3].time);
+    const persons = (trainingsHash[3].persons);
+    const content = (trainingsHash[3].content);
+    $('#selected-title').text(title);
+    $('#selected-time').text(time);
+    $('#selected-persons').text(persons);
+    $('#selected-content').text(content);
+  });
+  $('#training1').on('click',function(){
+    const title = (trainingsHash[0].title);
+    const time = (trainingsHash[0].time);
+    const persons = (trainingsHash[0].persons);
+    const content = (trainingsHash[0].content);
+    $('#selected-title').text(title);
+    $('#selected-time').text(time);
+    $('#selected-persons').text(persons);
+    $('#selected-content').text(content);
+  });
 })
